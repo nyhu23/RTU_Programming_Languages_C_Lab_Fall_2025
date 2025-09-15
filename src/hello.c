f94a797a87e3e8730e0211a7ace5d0c19248415a
nyhu23 <Nithya.Santhosh@edu.rtu.lv>
2025-09-15 08:06:06 +0000

    formatting by rtu std

--- src/hello.c
+++ src/hello.c
@@ -2,8 +2,8 @@
 #include <stdio.h>
 
 int main(int argc, char *argv[]) {
-  // This is your first C program my friend
-  printf("Hello, RTU World from C Lab in 2025!\n");
+  // this is not my first code
+  printf("Hello, my name is nithya!\n");
   printf("You passed %d argument(s).\n", argc - 1);
   for (int i = 1; i < argc; ++i) {
     printf("  arg[%d] = %s\n", i, argv[i]);
